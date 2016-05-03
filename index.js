de6069e744d2e0d6c7aac013990dacfb657dc713
Jason Cooper <jacooper7@gmail.com>
2016-05-03 06:44:09 +0000

    sucessfully loaded the function's env variables

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path  = require('path'),
   lambdaWrapper = require('lambda-wrapper'),
   Mocha = require('mocha'),
   chai = require('chai'),
+  Path = require('path'),
   BbPromise = require('bluebird'); // Serverless uses Bluebird Promises and we recommend you do to because they provide more than your average Promise :)
 
 const testFolder = 'test'; // Folder used my mocha for tests
@@ -114,7 +115,7 @@ module.exports = function(S) { // Always pass in the ServerlessPlugin Class
       return new BbPromise(function(resolve, reject) {
           let funcName = evt.options.paths;
           let mocha = new Mocha();
-          evt.options.paths.forEach(function(func, idx){ SetEnvVars(func); });
+          SetEnvVars(evt.options.paths)
 
           getFilePaths(evt.options.paths)
           .then(function(paths) {
@@ -123,9 +124,9 @@ module.exports = function(S) { // Always pass in the ServerlessPlugin Class
                 mocha.addFile(path);
               })
               mocha.run();
-              evt.options.paths.forEach(function(func, idx){ UnsetEnvVars(func); });
+              UnsetEnvVars(evt.options.paths)
           }, function(error) {
-            evt.options.paths.forEach(function(func, idx){ UnsetEnvVars(func); });
+            UnsetEnvVars(evt.options.paths)
 
             return reject(error);
           });
@@ -145,25 +146,30 @@ module.exports = function(S) { // Always pass in the ServerlessPlugin Class
     }
   }
 
-
   //Set environment variables
-  function SetEnvVars(funcName) {
-    var func = S.getProject().getFunction(funcName);
-    var envVars = func.environment
-    for (var key in Object.keys(envVars)) {
-      process.env[key] = envVars[key];
-    }
-    console.warn(process.env);
+  function SetEnvVars(paths) {
+    paths.forEach(function(path, idx){
+      var funcName = Path.basename(path, '.js')
+      var func = S.getProject().getFunction(funcName).toObjectPopulated();
+      var envVars = func.environment
+      var fields = Object.keys(envVars);
+      for (var key in fields) {
+        process.env[fields[key]] = envVars[fields[key]];
+      }  
+    });
   }
   
   //Unset environment variables
-  function UnsetEnvVars(funcName) {
-      var func = S.getProject().getFunction(funcName);
+  function UnsetEnvVars(paths) {
+    paths.forEach(function(path, idx){
+      var funcName = Path.basename(path, '.js')
+      var func = S.getProject().getFunction(funcName).toObjectPopulated();
       var envVars = func.environment
-      for (var key in Object.keys(envVars)) {
-        delete process.env[key];
+      var fields = Object.keys(envVars);
+      for (var key in fields) {
+        delete process.env[fields[key]];
       }
-      console.warn(process.env);
+    });
   }
   
   // Create the test folder
