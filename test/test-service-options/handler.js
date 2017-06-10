131b976f8d7ecb0d91885b1e1c9c18fd76bc1bc5
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-06-10 20:11:57 +0300

    add handler that fails if env vars are incorrect

--- test/test-service-options/handler.js
+++ test/test-service-options/handler.js
@@ -1,5 +1,9 @@
 'use strict';
 
+if(process.env.STAGE !== 'prod-stage-test') {
+  throw new Error('Environmental variable STAGE is not defined');
+}
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -9,7 +13,11 @@ module.exports.hello = (event, context, callback) => {
     }),
   };
 
-  callback(null, response);
+  if(process.env.STAGE !== 'prod-stage-test') {
+    return callback('Environmental variable STAGE is incorrect');
+  }
+
+  return callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
